d1edf4764e0d8f98db073be00b8330ec0d0fd79e
IscJacobo19 <iscjacobomanuel@gmail.com>
2020-07-14 21:30:45 -0500

    We add callback to our args file parser.rb We add the apexecute method that serves to have a record of the arguments and thus be able to call a function

--- app/lib/args_parser.rb
+++ app/lib/args_parser.rb
@@ -1,24 +1,40 @@
-# This class is a parser for analize the flags inputs
-# The flags 
+# frozen_string_literal: true
 
+# This class is a parser for analize the flags inputs
+# The flags
 class ArgsParser
-    attr_reader :arguments
-    def initialize arguments
-      @arguments = arguments
-      exit_value = nil
-      arguments.each do |flag|
-        case flag
-        when '-h' then exit_value = my_help_menu
-        when '--help' then exit_value = my_help_menu
-        else
-          exit_value = flag
+  attr_reader :arguments
+  attr_accessor :register
+
+  def initialize(arguments)
+    @arguments = arguments
+  end
+
+  def apexecute
+    @arguments.each_with_index do |args, i|
+      @register.each do |registro|
+        if args == registro[0]
+          apcallback(registro, @arguments[(i + 1)..(i + registro[1])])
+          # registro[2].call
         end
       end
-      exit_value
-    end
-  
-    def my_help_menu
-      return 'This is only for test purpose'
     end
   end
-  
+
+  private
+
+  def apcallback(argv, args)
+    # 0 = flag
+    # 1 = no. de argumentos de funcion
+    # 2 = callback
+
+    case argv[1]
+    when 0 then argv[2].call
+    when 1 then argv[2].call(args[0])
+    when 2 then argv[2].call(args[0], args[1])
+    when 3 then argv[2].call(args[0], args[1], args[2])
+    when 4 then argv[2].call(args[0], args[1], args[2], args[3])
+    when 5 then argv[2].call(args[0], args[1], args[2], args[4], args[5])
+    end
+  end
+end
--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 require_relative 'lib/args_parser'
 
 parser = ArgsParser.new(ARGV)
+parser.register = [
+  # The register is like next line
+  # ['-f', 1, method(:prueba)]
+  # for call a function can utilize method(:name-of-function)
+  # in case for call a method in a class, utilize NameOfClass.method(:name-of-method)
+]
+
+parser.apexecute
